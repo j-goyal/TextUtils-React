4dabe312886302a4be8688487557a867d3d864cb
j-goyal <jating07925@gmail.com>
2022-11-07 01:41:51 +0530

    Updates

---
+++ precache-manifest.29303f8290498032b4873ff1bd8425e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b4fa52104fb22de5119",
+    "url": "/TextUtils-React/static/css/main.27788b1b.chunk.css"
+  },
+  {
+    "revision": "1b4fa52104fb22de5119",
+    "url": "/TextUtils-React/static/js/main.3e28cdc2.chunk.js"
+  },
+  {
+    "revision": "118d5bb5acde121ee0e8",
+    "url": "/TextUtils-React/static/js/runtime~main.6ef670dd.js"
+  },
+  {
+    "revision": "9162cf41c7fa6eafdcbb",
+    "url": "/TextUtils-React/static/js/2.518c7694.chunk.js"
+  },
+  {
+    "revision": "cd9dcf72c3151b386634",
+    "url": "/TextUtils-React/static/js/3.3331c834.chunk.js"
+  },
+  {
+    "revision": "5972506e859119e4954b38bf984991e6",
+    "url": "/TextUtils-React/index.html"
+  }
+];
